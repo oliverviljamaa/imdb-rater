e06e3023f7bf4319f3a6b435c6f6dbfc8251a170
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2018-09-24 19:37:08 +0100

    Add ESLint, remove unnecessary boilerplate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <main>
+    <header>
+      <h1>IMDb rater</h1>
+    </header>
+  </main>
+);
 
 export default App;
